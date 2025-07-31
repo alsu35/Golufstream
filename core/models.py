9d0a7886bfedd8518204721ba72a39bbff348587
alsu35 <alsu73047@bk.ru>
2025-07-31 09:09:58 +0300

    correction create superuser

--- core/models.py
+++ core/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import ValidationError
 from django.core.cache import cache
@@ -90,7 +90,31 @@ class Department(models.Model):
         
     def __str__(self):
         return f"{self.organization.name} - {self.name}"
+    
+# --- Кастомный менеджер ---
+class CustomUserManager(BaseUserManager):
+    use_in_migrations = True
+    
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email обязателен')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
 
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Суперпользователь должен иметь is staff')
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Суперпользователь должен иметь is_superuser')
+        
+        return self.create_user(email, password, **extra_fields)
 # --- Пользователь ---
 class User(AbstractUser):
     username = models.CharField(
@@ -111,6 +135,8 @@ class User(AbstractUser):
     USERNAME_FIELD = 'email'  # логин по email
     REQUIRED_FIELDS = []       # username не обязателен
     
+    objects = CustomUserManager()
+
     def __str__(self):
         return self.email
 
